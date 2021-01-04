16a3d54ffc036e512da156836d1fe4edf6ab0d51
Oosjoost <joostensandy@gmail.com>
2021-01-04 20:00:39 +0100

    Natuurbrand lijnen (#41)
    * Natuurbrand - add lines and polygons to plugin
    
    Added lines and polgons as buttons, added to config DB and png files.
    Update version of plugin to 3.2.6
    
    * Natuurbrand - add lines and polygons to QGis project
    
    create layers and styles in project
    create symbology
    create gebiedgerichte aanpak validation
    update dimension db
    update version 3.2.6

--- plugin/oiv/oiv.py
+++ plugin/oiv/oiv.py
@@ -50,8 +50,8 @@ from .repressief_object.oiv_objectnieuw import oivObjectNieuwWidget
 class oiv:
     """initialize class attributes"""
 
-    compatibleVersion = [315, 324]
-    pluginVersion = '3.2.4'
+    compatibleVersion = [315, 326]
+    pluginVersion = '3.2.6'
     minBouwlaag = -10
     maxBouwlaag = 30
     checkVisibility = False
