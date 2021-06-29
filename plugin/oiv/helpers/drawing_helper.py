ea308b75cedf665c71157caf5eb5af842754825a
Oosjoost <joostensandy@gmail.com>
2021-06-29 14:39:36 +0200

    added new symbols to plugin
    added to forms
    added to resources
    added to config DB

--- plugin/oiv/helpers/drawing_helper.py
+++ plugin/oiv/helpers/drawing_helper.py
@@ -2,11 +2,12 @@
 import oiv.helpers.constants as PC
 #repressief object constants
 
-ROSNAPSYMBOLS = ['32', '47', '148', '150', '152', '301', 'Algemeen', 'Voorzichtig', 'Waarschuwing', 'Gevaar']
+ROSNAPSYMBOLS = ['32', '47', '148', '150', '152', '301',
+                 'Algemeen', 'Voorzichtig', 'Waarschuwing', 'Gevaar']
 ROSNAPLAYERS = ["Object terrein", "Isolijnen", "Bereikbaarheid", "Sectoren"]
 OBJECTTYPES = ['Evenement', 'Gebouw', 'Natuur', 'Waterongeval']
 
 #bouwlaag constants
 BLSNAPLAYERS = [PC.PAND["bouwlaaglayername"], "Bouwkundige veiligheidsvoorzieningen", "Ruimten"]
-BLSNAPSYMBOLS = ['1', '10', '32', '47', '148', '149', '150', '151', '152',
+BLSNAPSYMBOLS = ['1', '10', '32', '47', '148', '149', '150', '151', '152', '701', '702', '703', '704',
                  '1011', 'Algemeen', 'Voorzichtig', 'Waarschuwing', 'Gevaar']
