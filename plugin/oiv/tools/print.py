1e56506fb123f2704b7950da646e1be207c956ac
Oosjoost <joostensandy@gmail.com>
2021-12-13 15:37:50 +0100

    print atlas based on filter
    add bouwlagen A3 print pdf

--- plugin/oiv/tools/print.py
+++ plugin/oiv/tools/print.py
@@ -1,10 +1,29 @@
+import os
 import qgis.PyQt.QtCore as PQtC
 import qgis.core as QC
 import qgis.gui as QG
 
-def load_composer(layout_name, output_folder):
+def load_composer():
     project = QC.QgsProject.instance()
+    
+    layout_name = 'print_pdf_A3'
+    output_folder = 'D:/testprint/'
     layout = project.layoutManager().layoutByName(layout_name)
-    exporter = QC.QgsLayoutExporter(layout)
-    settings = QC.QgsLayoutExporter.ImageExportSettings()
-    exporter.exportToPdf(layout.atlas(), output_folder, 'tif', settings)
+
+    atlas = layout.atlas()
+    atlas.setFilterExpression('"object_id"=1004')
+    atlas.filterFeatures()
+    atlas.updateFeatures()
+    
+    print_atlas(layout, atlas, output_folder)
+
+def print_atlas(layout, atlas, output_folder):
+    if atlas.beginRender():
+        while atlas.next():
+            exporter = QC.QgsLayoutExporter(layout)
+            settings = QC.QgsLayoutExporter.PdfExportSettings()
+            fileName = atlas.currentFilename() + '.pdf'
+            print(fileName)
+            filePath = output_folder + fileName
+            exporter.exportToPdf(filePath, settings)
+        atlas.endRender()
