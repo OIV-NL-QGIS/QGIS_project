5eb30553fc8de50430c122fe6e8dac5901f13797
Oostjoost <joostensandy@gmail.com>
2023-08-09 11:54:12 +0200

    chagen color of dropdown selection
    resolves #376
    resolves #398

--- qgis_project/objecten/ui/applySave.py
+++ qgis_project/objecten/ui/applySave.py
@@ -1,8 +1,18 @@
-from qgis.PyQt.QtWidgets import QDialogButtonBox
+from qgis.PyQt.QtWidgets import QDialogButtonBox, QComboBox
+from qgis.gui import QgsEditorWidgetWrapper, QgsFilterLineEdit
 
 buttonBoxName = "buttonBox"
  
 def formOpen(dialog, layer, feature):
+
+    for wrapper in dialog.findChildren(QgsEditorWidgetWrapper):
+        if isinstance(wrapper.widget(), QComboBox):
+            wrapper.widget().setStyleSheet("color: rgb(0, 0, 0); background-color: lightgray;")
+
+    comboBoxes = dialog.findChildren(QComboBox)
+    for box in comboBoxes:
+        box.setStyleSheet("color: rgb(0, 0, 0); background-color: lightgray;")
+        
     okButton = dialog.findChild(QDialogButtonBox, buttonBoxName)
     bnOk = okButton.button(QDialogButtonBox.Ok)
     bnOk.clicked.connect(lambda: applySave(layer))
