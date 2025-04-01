aa157d3b81648c47a56e29715d2a882e6cce7259
Oostjoost <joostensandy@gmail.com>
2025-04-01 11:29:49 +0200

    Formaat verwerkt in Project
    resolves #514

--- qgis_project/objecten/ui/scenario.py
+++ qgis_project/objecten/ui/scenario.py
@@ -1,9 +1,19 @@
 import webbrowser
 import qgis.PyQt.QtWidgets as PQtW
+from qgis.gui import QgsEditorWidgetWrapper, QgsFilterLineEdit
 
 def formOpen(dialog, layer, feature):
     pushBtn = dialog.findChild(PQtW.QPushButton, "pushButton")
     pushBtn.clicked.connect(lambda: open_url(feature, dialog))
+
+    for wrapper in dialog.findChildren(QgsEditorWidgetWrapper):
+        if isinstance(wrapper.widget(), PQtW.QComboBox):
+            wrapper.widget().setStyleSheet("color: rgb(0, 0, 0); background-color: lightgray;")
+
+    comboBoxes = dialog.findChildren(PQtW.QComboBox)
+    for box in comboBoxes:
+        box.setStyleSheet("color: rgb(0, 0, 0); background-color: lightgray;")
+
     okButton = dialog.findChild(PQtW.QDialogButtonBox, "buttonBox")
     bnOk = okButton.button(PQtW.QDialogButtonBox.Ok)
     bnOk.clicked.connect(lambda: applySave(layer))
