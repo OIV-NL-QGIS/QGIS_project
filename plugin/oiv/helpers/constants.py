ac0aff933961258dc8be95a45c237f968b3dd961
Oostjoost <joostensandy@gmail.com>
2025-06-18 21:11:18 +0200

    Update installer and versions

--- plugin/oiv/helpers/constants.py
+++ plugin/oiv/helpers/constants.py
@@ -36,14 +36,14 @@ def bagpand_layername():
     return None
 
 
-OIV_VERSION = '3.6.6'
+OIV_VERSION = '3.6.8'
 
 PLUGIN = {
     "name": "OIV Objecten",
     "toolbartext": "OIV " + OIV_VERSION + " | Actieve bouwlaag: ",
     "compatibleDbVersion": {
-        "min": 362,
-        "max": 366
+        "min": 364,
+        "max": 368
     },
     "menulocation": "&OIV Objecten",
     "settingsname": "Configure",
